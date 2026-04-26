473ddae314af130a6bfae6a9855fd22a155cd6f8
Da Evil <123114180+curfewUnZipper@users.noreply.github.com>
2026-04-26 11:17:17 +0530

    added backend uri

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -4,8 +4,8 @@ import { Observable } from 'rxjs';
 
 /* ================= CONFIG ================= */
 // 👉 CHANGE THIS ONE LINE LATER
-const BASE_URL = 'http://localhost:8080';
-// const BASE_URL = 'https://your-backend-url.com';
+// const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://splitsbillahhdocker.onrender.com';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
